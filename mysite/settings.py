fc95fef9899eb75918e8d94815e28371fe7d1a12
l769829723 <769829723@qq.com>
2016-07-16 10:33:39 +0800

    update celery agent module

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,24 +35,9 @@ def localhost():
         return True
 
 if localhost():
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
     # Debug mode button
     DEBUG = True
 else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'mysite',
-            'HOST': 'licy.mysql.pythonanywhere-services.com',
-            'USER': 'licy',
-            'PASSWORD': 'lcy2016Happ',
-        }
-    }
     # Debug mode button
     DEBUG = False
     ALLOWED_HOSTS = '*'
@@ -79,6 +64,9 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -100,18 +88,25 @@ TEMPLATES = [
     },
 ]
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'mysite_cache_table',
+    }
+}
+
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
--- v1/task.py
+++ v1/task.py
@@ -1,11 +1,13 @@
 from __future__ import absolute_import
 
 from celery import shared_task
+import time
 
 
 @shared_task
 def add(x, y):
     print('*** arg: %d + arg: %d ***' % (x, y))
+    time.sleep(10)
     return x + y
 
 
